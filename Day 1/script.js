b91c5b53ccf4add0622d9eeac34634cdc8d19407
Abhiroop <zupalliabhiroop@gmail.com>
2026-06-23 22:45:48 +0530

    initial commit

---
+++ Day 1/script.js
@@ -0,0 +1,78 @@
+// console.log("Hello");
+// console.log("Abhi");
+// console.log("JS");
+//  num=2
+//  console.log(num)
+
+// console.log(4+6);
+
+// var num=45;
+// var num=55;
+// console.log(num);
+
+// let a=34;
+// a=23;
+// console.log(a);
+
+// const a=45;
+// console.log(a);
+
+// var n=23;
+// var b=23;
+// console.log(n,b);
+
+// let v=6;
+// let b=8;
+// console.log(v,b);
+
+// const a=8;
+// const c=8;
+// console.log(a,c);
+
+// ES6 - ECMA script 2015
+
+// a=10;
+// console.log(typeof a);
+
+// a=12.334;
+// console.log(typeof(a));
+
+// a=false;
+// console.log(typeof a);
+
+// a="abhi";
+// console.log(typeof a);
+
+// let a=23;
+// let b=20;
+// if (a >b ){
+//     console.log("Boochi");
+
+// } else {
+//     console.log("booo");
+// }
+
+
+// let a=24;
+// let b=25;
+// if (a < b){
+//     console.log("b is greater value");
+// } else if (a > b ){
+//     console.log("a is greater");
+// } else {
+//     console.log("b is greater");
+// }
+
+// for(let a=1; a<10; a++){
+// console.log(a);
+// }
+
+
+// a=5;
+// b=10;
+// c=a+b;
+// console.log(c);
+
+var a=34;
+var a=35;
+console.log(a);
---
+++ Day 2/script.js
@@ -0,0 +1,68 @@
+// Functions
+
+// function myFun(a,b) {
+//     console.log(a-b);
+// }
+
+// const fun1 = function (a,b) {
+//     console.log(a+b);
+// }
+
+// const fun2 = (a,b)  => {
+//     console.log(a*b);
+//     return a+b
+// }
+
+// const fun3 = (a,b) => a + b;
+// res=fun3(5,8);
+//     console.log(res);
+
+// myFun(3,1)
+// fun1(2,4)
+// fun2(3,3)
+// fun3(4,9)
+
+
+// Arrays
+
+// let data1 = [10, 5234.231, "hello", true];
+// let data2 = [100, 200, 300];
+// let newData =[11, ...data1, 30, ...data2];
+// console.log(newData);
+
+// let data =[10, 5234.231, "hello", true];
+// for (let i=0; i<data.length; i++) {
+//     console.log(data[i]);
+// }
+
+// data.push(100)
+// data.pop()
+
+// data.unshift(100)
+// data.shift()
+
+
+
+// Objects
+
+let data =[
+    {
+        id:125,
+        name:"Abhi",
+        age:23,
+        techstack : ["Python","Django","React"]
+    },
+    {
+        id:123,
+        name:["Nagi","mma","mj"]
+        age:23
+    },
+    {
+        id:100,
+        name:"Kethan",
+        age:23
+
+    }
+]
+console.log(data[0].techstack[0]);
+console.log(data[1].age)
