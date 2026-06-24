3cf3adb14df2ba92a32014f6ae020897c68b7f1a
Abhiroop <zupalliabhiroop@gmail.com>
2026-06-24 10:18:15 +0530

    Added few files

---
+++ Day 3/script.js
@@ -0,0 +1,37 @@
+// DOM  (Document object model)
+
+// window - BOM  (Browser object model)
+
+//         Document -DOM
+//               console
+//                    log
+
+// console.log(document);
+// console.log(document.head);
+// console.log(document.body)
+
+// let para=document.getElementsByTagName('p')
+// console.log(para)
+
+// let heading=document.getElementsByTagName('h1')
+// console.log(heading)
+
+
+// let display = document.getElementById("display")
+// console.log(display);
+// console.log(display.innerText)
+// display.innerText="Abhiroop"
+
+// let heading =document.getElementsByTagName('h1')
+// heading[0].innerText="this is heading"
+
+
+
+
+const change =() => {
+    let display=document.getElementById("hello")
+    display.innerHTML="BYEE"
+
+}
+
+
